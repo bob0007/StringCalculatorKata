43c48623497a4e49f75f315d34585e51d641a67a
bob0007 <henrybirgelee@yahoo.com>
2015-03-02 15:28:03 -0800

    Extended tests and implemented string calculator.

--- tests/calculatorkata/StringCalculatorTest.java
+++ tests/calculatorkata/StringCalculatorTest.java
@@ -28,4 +28,122 @@ public class StringCalculatorTest {
 		
 		Assert.assertEquals(1, result);
 	}
+	
+	@Test
+	public void Run_AdditionString_ReturnsTwo()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("1 + 1");
+		
+		Assert.assertEquals(2, result);
+	}
+	
+	@Test
+	public void Run_OnlyWhiteSpace_ReturnsZero()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("   \n");
+		
+		Assert.assertEquals(0, result);
+	}
+	
+	@Test
+	public void Run_DivisionString_ReturnsOne()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("3 / 3");
+		
+		Assert.assertEquals(1, result);
+	}
+	
+	@Test
+	public void Run_ModuloString_ReturnsOne()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("4 % 3");
+		
+		Assert.assertEquals(1, result);
+	}
+	
+	@Test
+	public void Run_MultiOperationString_ReturnsTen()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("3 * 4 / 3 + 5 + 1 * 1");
+		
+		Assert.assertEquals(10, result);
+	}
+	
+	@Test
+	public void Run_GroupedOperatorString_ReturnsTwo()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("16 / (4 + 4)");
+		
+		Assert.assertEquals(2, result);
+	}
+	
+	@Test
+	public void Run_DoubleGroupedOperatorString_ReturnsTwo()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("(1+1) * (3 -2)");
+		
+		Assert.assertEquals(2, result);
+	}
+	
+	@Test
+	public void Run_NestedParensString_ReturnsEight()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("(1*(1 + 7)) * (3 -2)");
+		
+		Assert.assertEquals(8, result);
+	}
+	
+	@Test
+	public void Run_PowerString_ReturnsFour()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("2 ^  2");
+		
+		Assert.assertEquals(4, result);
+	}
+	
+	@Test
+	public void Run_MultiOperatorPowerString_ReturnsFive()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("2 ^  2 + 1");
+		
+		Assert.assertEquals(5, result);
+	}
+	
+	@Test
+	public void Run_NegativeString_ReturnsNegativeOne()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run("  - 1");
+		
+		Assert.assertEquals(-1, result);
+	}
+	
+	@Test
+	public void Run_UnaryMinusString_ReturnsOne()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run(" 2+ - 1");
+		
+		Assert.assertEquals(1, result);
+	}
+	
+	@Test
+	public void Run_LongOperatorString_ReturnsTwo()
+	{
+		StringCalculator calculator = new StringCalculator();
+		int result = calculator.run(" 3 * 4 / 10 * 1 + .8");
+		
+		Assert.assertEquals(2, result);
+	}
+	
 }
